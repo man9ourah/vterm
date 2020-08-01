5dadf529a2f405c836d49f565caa193eca951b79
mansourah <malharthi9@gatech.edu>
2020-08-01 12:29:47 -0400

    add comment and normal keybinding

--- src/v_keybindings.h
+++ src/v_keybindings.h
@@ -5,79 +5,101 @@
 
 /*
  * VTerm keybindings
+ * ALL KEYBINDINGS BELOW ARE TO WORK WITH THE MODIFIER
  */
 
 /*
  * New tab
+ * (WITH_MODIFIER)
  */
 #define VKEY_NEW_TAB GDK_KEY_t
 
 /*
  * Next tab
+ * (WITH_MODIFIER)
  */
 #define VKEY_NEXT_TAB GDK_KEY_l
 
 /*
  * Previous tab
+ * (WITH_MODIFIER)
  */
 #define VKEY_PREV_TAB GDK_KEY_h
 
 /*
  * Alternate tab
+ * (WITH_MODIFIER)
  */
 #define VKEY_ALTERNATE_TAB GDK_KEY_asciicircum
 
 /*
  * Fast access 1
+ * (WITH_MODIFIER)
  */
 #define VKEY_FAST_ACCESS_1 GDK_KEY_exclam
 
 /*
  * Fast access 2
+ * (WITH_MODIFIER)
  */
 #define VKEY_FAST_ACCESS_2 GDK_KEY_at
 
 /*
  * Fast access 3
+ * (WITH_MODIFIER)
  */
 #define VKEY_FAST_ACCESS_3 GDK_KEY_numbersign
 
 /*
  * VTab keybindings
+ * ALL KEYBINDINGS BELOW ARE TO WORK WITH THE MODIFIER
  */
 
 /*
+ * Toggle normal mode
+ * (WITH_MODIFIER)
+ */
+#define VKEY_TOGGLE_NORMAL GDK_KEY_space
+
+/*
  * Close tab
+ * (WITH_MODIFIER)
  */
 #define VKEY_CLOSE_TAB GDK_KEY_w
 
 /*
  * Up prompt
+ * (WITH_MODIFIER)
  */
 #define VKEY_UP_PROMPT GDK_KEY_i
 
 /*
  * Down prompt
+ * (WITH_MODIFIER)
  */
 #define VKEY_DOWN_PROMPT GDK_KEY_o
 
 /*
  * Move tab to right
+ * (WITH_MODIFIER)
  */
 #define VKEY_MOVE_TAB_RIGHT GDK_KEY_k
 
 /*
  * Move tab to left
+ * (WITH_MODIFIER)
  */
 #define VKEY_MOVE_TAB_LEFT GDK_KEY_j
 
 /*
  * Copy selected text to clipboard
+ * (WITH_MODIFIER)
  */
 #define VKEY_COPY GDK_KEY_c
 
 /*
  * Paste text in clipboard
+ * (WITH_MODIFIER)
  */
 #define VKEY_PASTE GDK_KEY_v
 
